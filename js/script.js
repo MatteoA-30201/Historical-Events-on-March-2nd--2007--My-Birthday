a08bed9a43ceb7efdcc910f0e73052e498f8e57d
Matteo <125mamatucci@frhsd.com>
2024-03-05 08:38:08 -0500

    ef yt4ergy

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const imgChangeJS = document.getElementById("img-change");
+const buttonJS = document
+
+let textVerify1 = document.getElementById("button-id-1").textContent; 
+let textVerify2 = document.getElementById("button-id-2").textContent; 
+let textVerify3 = document.getElementById("button-id-3").textContent; 
+let textVerify4 = document.getElementById("button-id-4").textContent; 
+let textVerify5 = document.getElementById("button-id-5").textContent; 
+
+buttonJS.addEventListener("click", changeImgFunction);
+
+function changeImgFunction() {
+  
+}
+
+console.log(textVerify1);
