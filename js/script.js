ecc7424fde0d07366eaba3092c0ba3765fa7b0dc
MatteoA-30201 <113131451+MatteoA-30201@users.noreply.github.com>
2024-03-05 21:52:01 +0000

    fixed button resizing, and added button js support to switch imgs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
+/* 
+let buttonJS = document.getElementsByClassName("button-id");
+
 const imgChangeJS = document.getElementById("img-change");
-const buttonJS = document
 
-let textVerify1 = document.getElementById("button-id-1").textContent; 
-let textVerify2 = document.getElementById("button-id-2").textContent; 
-let textVerify3 = document.getElementById("button-id-3").textContent; 
-let textVerify4 = document.getElementById("button-id-4").textContent; 
-let textVerify5 = document.getElementById("button-id-5").textContent; 
+const buttonJS = document.querySelectorAll("button-id").forEach(item => {
+  buttonJS.addEventListener('click', event => {
+    if (textVerify.innerHTML === " Mikhail Gorbachev ") {
+      imgChangeJS.src = "Photos/Mikhail_Gorbachev.png";
+    } else if (textVerify.innerHTML === " Top Quark ") {
+      imgChangeJS.src = "Photos/Top_antitop_quark_event.svg.png";
+    } else if (textVerify.innerHTML === " King Kong ") {
+      imgChangeJS.src = "Photos/King_Kong.png";
+    } else if (textVerify.innerHTML === " Dr. Seuss ") {
+      imgChangeJS.src = "Photos/Dr_Seuss.png";
+    } else if (textVerify.innerHTML === " Rhodesia ") {
+      imgChangeJS.src = "Photos/Rhodesia.png";
+    }
+  })
+})
+*/
 
-buttonJS.addEventListener("click", changeImgFunction);
+let button = document.getElementById("button-id");
 
-function changeImgFunction() {
-  
+const imgChangeJS = document.getElementById("img-change");
+
+function functionImgChange1() {
+  imgChangeJS.src = "Photos/Mikhail_Gorbachev.jpg";
 }
 
-console.log(textVerify1);
+function functionImgChange2() {
+  imgChangeJS.src = "Photos/Top_antitop_quark_event.svg.png";
+}
+
+function functionImgChange3() {
+  imgChangeJS.src = "Photos/King_Kong.png";
+}
+
+function functionImgChange4() {
+  imgChangeJS.src = "Photos/Dr_Seuss.jpg";
+}
+
+function functionImgChange5() {
+  imgChangeJS.src = "Photos/Rhodesia.png";
+}   
